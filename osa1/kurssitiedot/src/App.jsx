5fbffae78f1eeba4462e5605822d93231add93f4
aphuus <aphuus@utu.fi>
2024-02-11 21:52:48 +0200

    osa1 - kurssitiedot

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,55 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </>
+  );
+};
+
+export default App;
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {course.parts.map((item) => (
+        <Part key={item.name} name={item.name} exercises={item.exercises} />
+      ))}
+    </>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ course }) => {
+  return <p>Number of exercises {course.parts.reduce((acc, item) => acc + item.exercises, 0)}</p>;
+};
