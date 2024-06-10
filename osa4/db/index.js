c5e298e21f96858fb206546b73ad827334825e06
aphuus <aphuus@utu.fi>
2024-06-10 23:18:50 +0300

    test ready

--- osa4/db/index.js
+++ osa4/db/index.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
+  console.log("Connecting to MongoDB...");
   try {
-    const mongoUrl = process.env.MONGODB_URL;
+    const mongoUrl =
+      process.env.NODE_ENV === "test"
+        ? process.env.TEST_MONGODB_URI
+        : process.env.MONGODB_URI;
     await mongoose.connect(mongoUrl);
-    console.log('MongoDB connected...');
+    console.log("MongoDB connected...");
   } catch (error) {
-    console.error('Database connection error:', error);
+    console.error("Database connection error:", error);
     process.exit(1);
   }
 };
